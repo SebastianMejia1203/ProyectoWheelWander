2e7e132b813933cfeafb4be39e37c49acd772be6
Sebastian Mejia <okashikonen@gmail.com>
2024-04-20 16:29:41 -0500

    inicio casi terminado

--- ProyectoWheelWander/wwwroot/js/site.js
+++ ProyectoWheelWander/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+// Funciones para los botones
+function loginFunction() {
+    console.log("Login clicked");
+    // Implementa la función de login aquí
+}
+
+function registerFunction() {
+    console.log("Register clicked");
+    // Implementa la función de registro aquí
+}
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+
+})
